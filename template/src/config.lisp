31b1357f4d64bef687ec1618adace76a7b307deb
Manuel Odendahl <wesen@ruinwesen.com>
2010-09-21 23:10:08 +0200

    add asdf path thingie to config.lisp

--- template/src/config.lisp
+++ template/src/config.lisp
@@ -1,12 +1,12 @@
 (in-package :template)
 
-(eval-when (:compile-toplevel)
-	(defparameter *config-file-pathname* *compile-file-truename*))
+;; this is kind of a dirty hack to get the root of the thirdparty directory
+(defparameter *thirdparty-directory*
+  (asdf:system-relative-pathname :cl-gd #p"../"))
 
-(defparameter *website-url* "http://localhost:4242/")
-(defparameter *root-directory* (merge-pathnames #p"../"
-																								 (make-pathname :name nil :type nil
-																																:version nil :defaults *config-file-pathname*)))
+(defparameter *website-url* "http://portfolio.ruinwesen.com/")
+(defparameter *root-directory* (asdf:system-relative-pathname :quickhoney #p"../"))
+
 (defparameter *store-directory* (merge-pathnames #p"datastore/" *root-directory*))
 (defparameter *website-directory* (probe-file (merge-pathnames #p"website/" *root-directory*)))
 (defparameter *webserver-port* 4242)
