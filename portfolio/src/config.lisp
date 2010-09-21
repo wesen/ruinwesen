33e4b212007bcc03074a208ddf4d6dddea71235b
Manuel Odendahl <wesen@ruinwesen.com>
2010-09-21 23:12:55 +0200

    Fix paths

--- portfolio/src/config.lisp
+++ portfolio/src/config.lisp
@@ -2,21 +2,20 @@
 
 (eval-when (:compile-toplevel :Execute)
   
-	(defparameter *config-file-pathname* "/home/manuel/code/wesen/ruinwesen/portfolio/src/config.lisp")
+  (defparameter *config-file-pathname* "/home/manuel/code/wesen/ruinwesen/portfolio/src/config.lisp")
   (defparameter bknr.web::*template-dtd-catalog* (list "/home/manuel/code/lisp/bknr-svn/thirdparty/xhtml/catalog.xml"))
-
+  
   (setf cxml:*catalog* (cxml:make-catalog *template-dtd-catalog*)
 	cxml:*dtd-cache* (cxml:make-dtd-cache)
-	cxml:*cache-all-dtds* t)
-
-  )
+	cxml:*cache-all-dtds* t))
   
+;; this is kind of a dirty hack to get the root of the thirdparty directory
+(defparameter *thirdparty-directory*
+  (asdf:system-relative-pathname :cl-gd #p"../"))
 
+(defparameter *website-url* "http://portfolio.ruinwesen.com/")
+(defparameter *root-directory* (asdf:system-relative-pathname :quickhoney #p"../"))
 
-(defparameter *website-url* "http://localhost:4242/")
-(defparameter *root-directory* (merge-pathnames #p"../"
-																								 (make-pathname :name nil :type nil
-																																:version nil :defaults *config-file-pathname*)))
 (defparameter *store-directory* (merge-pathnames #p"datastore/" *root-directory*))
 (defparameter *website-directory* (probe-file (merge-pathnames #p"website/" *root-directory*)))
 (defparameter *webserver-port* 4242)
