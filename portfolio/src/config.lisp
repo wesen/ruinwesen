f0fbe818f6060535baefe1dd736542b3b6790214
Manuel Odendahl <wesen@ruinwesen.com>
2010-10-04 19:13:00 +0200

    Add new fangled directory lookup to config

--- portfolio/src/config.lisp
+++ portfolio/src/config.lisp
@@ -1,21 +1,13 @@
 (in-package :portfolio)
 
-(eval-when (:compile-toplevel :Execute)
-  
-  (defparameter *config-file-pathname* "/home/manuel/code/wesen/ruinwesen/portfolio/src/config.lisp")
-  (defparameter bknr.web::*template-dtd-catalog* (list "/home/manuel/code/lisp/bknr-svn/thirdparty/xhtml/catalog.xml"))
-  
-  (setf cxml:*catalog* (cxml:make-catalog *template-dtd-catalog*)
-	cxml:*dtd-cache* (cxml:make-dtd-cache)
-	cxml:*cache-all-dtds* t))
-  
 ;; this is kind of a dirty hack to get the root of the thirdparty directory
 (defparameter *thirdparty-directory*
   (asdf:system-relative-pathname :cl-gd #p"../"))
 
 (defparameter *website-url* "http://portfolio.ruinwesen.com/")
-(defparameter *root-directory* (asdf:system-relative-pathname :quickhoney #p"../"))
+(defparameter *root-directory* (asdf:system-relative-pathname :portfolio #p"../"))
+(defparameter *xml-catalog-directory* (asdf:system-relative-pathname :portfolio #p"../xml/"))
 
 (defparameter *store-directory* (merge-pathnames #p"datastore/" *root-directory*))
 (defparameter *website-directory* (probe-file (merge-pathnames #p"website/" *root-directory*)))
-(defparameter *webserver-port* 4242)
+(defparameter *webserver-port* 4343)
