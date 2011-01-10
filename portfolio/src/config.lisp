4a5d23e3924d1cc4aafbd5ecbbccb80dcf926ddf
Manuel Odendahl <wesen@ruinwesen.com>
2011-01-10 12:15:20 +0000

    foobar

--- portfolio/src/config.lisp
+++ portfolio/src/config.lisp
@@ -2,7 +2,7 @@
 
 (eval-when (:compile-toplevel :Execute)
   
-	(defparameter *config-file-pathname* "/home/manuel/code/wesen/ruinwesen/portfolio/src/config.lisp")
+	(defparameter *config-file-pathname* (asdf:system-relative-pathname :portfolio #p"config.lisp"))
   (defparameter bknr.web::*template-dtd-catalog* (list "/home/manuel/code/lisp/bknr-svn/thirdparty/xhtml/catalog.xml"))
 
   (setf cxml:*catalog* (cxml:make-catalog *template-dtd-catalog*)
@@ -14,9 +14,7 @@
 
 
 (defparameter *website-url* "http://localhost:4242/")
-(defparameter *root-directory* (merge-pathnames #p"../"
-																								 (make-pathname :name nil :type nil
-																																:version nil :defaults *config-file-pathname*)))
+(defparameter *root-directory* (asdf:system-relative-pathname :portfolio #p"../"))
 (defparameter *store-directory* (merge-pathnames #p"datastore/" *root-directory*))
 (defparameter *website-directory* (probe-file (merge-pathnames #p"website/" *root-directory*)))
 (defparameter *webserver-port* 4242)
